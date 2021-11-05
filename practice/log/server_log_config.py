c7667c338be22a3d6c4602b0e4f1725462a2ee46
visor517 <visor517@yandex.ru>
2021-11-05 13:16:50 +0300

    added server logs

---
+++ practice/log/server_log_config.py
@@ -0,0 +1,27 @@
+import logging
+import logging.handlers
+import os
+
+
+LOGGER = logging.getLogger('server')
+
+formatter = logging.Formatter(
+    "%(asctime)s %(levelname)-10s %(filename)s %(message)s")
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+PATH = os.path.join(PATH, 'server.log')
+
+fh = logging.handlers.TimedRotatingFileHandler(
+    PATH, encoding='utf8', interval=1)
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+
+LOGGER.addHandler(fh)
+LOGGER.setLevel(logging.DEBUG)
+
+if __name__ == '__main__':
+    LOGGER.critical('Критическая ошибка')
+    LOGGER.error('Ошибка')
+    LOGGER.warning('Предупреждение')
+    LOGGER.debug('Отладочная информация')
+    LOGGER.info('Информационное сообщение')
